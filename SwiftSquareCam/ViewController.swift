e7483a694f839dc8f8cf2b62cb689727f9ea92fb
JoppeSchwartz <joppeschwartz@gmail.com>
2014-09-23 08:31:35 -0400

    Initial Commit

---
+++ SwiftSquareCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSquareCam
+//
+//  Created by Joseph Schwartz on 9/23/14.
+//  Copyright (c) 2014 Joseph Schwartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
